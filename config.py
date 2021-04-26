a62741e1c85270402961e16fc18b0de0d870ca0f
yanqian <yanqian0716@gmail.com>
2021-04-26 15:40:17 +0800

    .

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ DTYPE = tf.float32
 # dataset
 RESIZE = 36  # 512
 CROP = 32  # 384
-ROOT_PATH = '../cifar'
-TRAIN_PATH = '../input/cifar10/cifar/train.csv'  # kaggle
-# TRAIN_PATH = '/content/cifar/train.csv'  # google
+ROOT_PATH = ''
+# TRAIN_PATH = '../input/cifar10/cifar/train.csv'  # kaggle
+TRAIN_PATH = '/content/cifar/train.csv'  # google
 BATCH_SIZE = 32  # 512
 SHUFFLE_BUFFER = 50000
 
@@ -16,16 +16,16 @@ NUM_CLASSES = 10
 # train
 BASE_LR = 0.0003
 TOTAL_EPOCHS = 100
-CONTINUE = True
+CONTINUE = False
 START_EPOCH = 0
 
 
 # test
-TEST_PATH = '../input/cifar10/cifar/test.csv'  # kaggle
-# TEST_PATH = '/content/cifar/test.csv'  # google
-LOAD_PATH = '../input/weights/weights/M'
+# TEST_PATH = '../input/cifar10/cifar/test.csv'  # kaggle
+TEST_PATH = '/content/cifar/test.csv'  # google
+LOAD_PATH = '/content/drive/MyDrive/weights/Invo'
 
 # evaluate
 LOG_EPOCH = 2
 LOG_LOSS = 200
-SAVE_PATH = './weights/M'
+SAVE_PATH = '/content/drive/MyDrive/weights/Invo'
